26bbc7e8e7393afc04edf7512d0a5bfef6b84013
ga58lar <dominik.kulmer@tum.com>
2025-01-30 21:34:20 +0100

    cmake fixes?

--- cmake/FindTENSORRT.cmake
+++ cmake/FindTENSORRT.cmake
@@ -1,39 +1,68 @@
-# Based on https://github.com/tier4/tensorrt_cmake_module
-if(DEFINED ENV{TENSORRT_ROOT})
-  set(TENSORRT_ROOT $ENV{TENSORRT_ROOT})
-endif()
-
-if(NOT TENSORRT_ROOT AND CUDA_TOOLKIT_ROOT_DIR)
-  set(TENSORRT_ROOT "${CUDA_TOOLKIT_ROOT_DIR}")
+# cmake/FindTENSORRT.cmake
+if(NOT DEFINED ENV{TENSORRT_ROOT} AND NOT DEFINED TENSORRT_ROOT)
+  if(EXISTS "/usr/include/x86_64-linux-gnu/NvInfer.h")
+    set(TENSORRT_ROOT "/usr/include/x86_64-linux-gnu")
+  elseif(EXISTS "/usr/local/cuda/include/NvInfer.h")
+    set(TENSORRT_ROOT "/usr/local/cuda")
+  endif()
 endif()
 
 find_path(TENSORRT_INCLUDE_DIR NvInfer.h
-  HINTS ${TENSORRT_ROOT} /usr/include /usr/local/include
-  PATH_SUFFIXES include)
+  HINTS 
+    ${TENSORRT_ROOT}
+    /usr/include/x86_64-linux-gnu
+    /usr/local/cuda/include
+    /usr/include
+  NO_DEFAULT_PATH
+)
 
 find_library(TENSORRT_LIBRARY_INFER nvinfer
-  HINTS ${TENSORRT_ROOT} /usr/lib/x86_64-linux-gnu /usr/local/lib
-  PATH_SUFFIXES lib lib64)
+  HINTS
+    /usr/lib/x86_64-linux-gnu
+    /usr/local/cuda/lib64
+    ${TENSORRT_ROOT}/lib
+    ${TENSORRT_ROOT}/lib64
+  NO_DEFAULT_PATH
+)
 
-find_library(TENSORRT_LIBRARY_ONNX nvonnxparser
-  HINTS ${TENSORRT_ROOT} /usr/lib/x86_64-linux-gnu /usr/local/lib
-  PATH_SUFFIXES lib lib64)
+find_library(TENSORRT_LIBRARY_ONNXPARSER nvonnxparser
+  HINTS
+    /usr/lib/x86_64-linux-gnu
+    /usr/local/cuda/lib64
+    ${TENSORRT_ROOT}/lib
+    ${TENSORRT_ROOT}/lib64
+  NO_DEFAULT_PATH
+)
 
-set(TENSORRT_INCLUDE_DIRS ${TENSORRT_INCLUDE_DIR})
-set(TENSORRT_LIBRARIES ${TENSORRT_LIBRARY_INFER} ${TENSORRT_LIBRARY_ONNX})
-
-# Version detection from NvInfer.h
-if(EXISTS "${TENSORRT_INCLUDE_DIR}/NvInfer.h")
-  file(READ "${TENSORRT_INCLUDE_DIR}/NvInfer.h" NVINFER_H_CONTENTS)
-  string(REGEX MATCH "define NV_TENSORRT_MAJOR ([0-9]+)" _ "${NVINFER_H_CONTENTS}")
-  set(TENSORRT_VERSION_MAJOR ${CMAKE_MATCH_1})
-  message(STATUS "Found TensorRT version: ${TENSORRT_VERSION_MAJOR}")
+if(TENSORRT_INCLUDE_DIR AND EXISTS "${TENSORRT_INCLUDE_DIR}/NvInfer.h")
+  file(READ "${TENSORRT_INCLUDE_DIR}/NvInfer.h" TENSORRT_HEADER_CONTENT)
+  string(REGEX MATCH "define NV_TENSORRT_MAJOR ([0-9]+)" _ "${TENSORRT_HEADER_CONTENT}")
+  
+  if(CMAKE_MATCH_1)
+    set(TENSORRT_VERSION_MAJOR ${CMAKE_MATCH_1})
+  else()
+    # Fallback for older TensorRT versions
+    set(TENSORRT_VERSION_MAJOR 7)
+    message(STATUS "Could not determine TensorRT version from header, assuming ${TENSORRT_VERSION_MAJOR}")
+  endif()
 endif()
 
+set(TENSORRT_INCLUDE_DIRS ${TENSORRT_INCLUDE_DIR})
+set(TENSORRT_LIBRARIES ${TENSORRT_LIBRARY_INFER} ${TENSORRT_LIBRARY_ONNXPARSER})
+
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(TENSORRT DEFAULT_MSG
-  TENSORRT_INCLUDE_DIR
-  TENSORRT_LIBRARY_INFER
-  TENSORRT_LIBRARY_ONNX
-  TENSORRT_VERSION_MAJOR
+find_package_handle_standard_args(TENSORRT
+  REQUIRED_VARS
+    TENSORRT_INCLUDE_DIR
+    TENSORRT_LIBRARY_INFER
+    TENSORRT_LIBRARY_ONNXPARSER
+    TENSORRT_VERSION_MAJOR
 )
+
+if(NOT TARGET TensorRT::TensorRT)
+  add_library(TensorRT::TensorRT INTERFACE IMPORTED)
+  set_target_properties(TensorRT::TensorRT PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${TENSORRT_INCLUDE_DIRS}"
+    INTERFACE_LINK_LIBRARIES "${TENSORRT_LIBRARIES}"
+  )
+endif()
